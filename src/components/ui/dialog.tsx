6f40dc7626dd71df0972268bd74c671168d8d180
salman mehmood <salmanmehmood613@gmail.com>
2026-01-16 17:14:39 +0500

    fix width of dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -47,6 +47,11 @@ const DialogContent = React.forwardRef<
         "md:data-[state=closed]:slide-out-to-left-1/2 md:data-[state=closed]:slide-out-to-top-[48%]",
         "md:data-[state=open]:slide-in-from-left-1/2 md:data-[state=open]:slide-in-from-top-[48%]",
         "md:data-[state=closed]:zoom-out-95 md:data-[state=open]:zoom-in-95",
+        // Large Desktop: Centered modal style full width
+        "lg:inset-auto lg:left-[50%] lg:top-[50%] lg:max-h-[85vh] lg:max-w-[1440px] md:translate-x-[-50%] md:translate-y-[-50%] md:rounded-lg",
+        "md:data-[state=closed]:slide-out-to-left-1/2 md:data-[state=closed]:slide-out-to-top-[48%]",
+        "md:data-[state=open]:slide-in-from-left-1/2 md:data-[state=open]:slide-in-from-top-[48%]",
+        "md:data-[state=closed]:zoom-out-95 md:data-[state=open]:zoom-in-95",
         className,
       )}
       {...props}
